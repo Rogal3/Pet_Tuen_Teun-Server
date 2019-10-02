2bb731c0098f1473fb12e99052c518153834a484
sungyil <shfur2006@gmail.com>
2019-10-03 00:09:27 +0900

    first commit

---
+++ src/main/java/view/exController.java
@@ -0,0 +1,16 @@
+package view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class exController {
+	@RequestMapping(value="/home",method=RequestMethod.GET)
+	public String loadHome(Model m) {
+		m.addAttribute("name","home!!!!");
+		System.out.println("asdasdf");
+		return "home";
+	}
+}
