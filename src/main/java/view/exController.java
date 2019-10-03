2a30a21bf0bbe4764027dd77c0bfacaf90d17550
ROGAL <ROGAL@DESKTOP-BS7OKCV>
2019-10-04 01:46:17 +0900

    FIRST COMMIT TEST

--- src/main/java/view/exController.java
+++ src/main/java/view/exController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class exController {
 	@RequestMapping(value="/home",method=RequestMethod.GET)
 	public String loadHome(Model m) {
-		m.addAttribute("name","asdfasdfasdfasfd");
+		m.addAttribute("name","for commit test");
 		
 		
 		return "home";
