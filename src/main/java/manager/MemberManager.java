f876d1fc9ca6583b0d281e612d6e0d1d5efe2e7e
sungyil <shfur2006@gmail.com>
2019-10-26 16:41:58 +0900

    manager

--- src/main/java/manager/MemberManager.java
+++ src/main/java/manager/MemberManager.java
@@ -43,5 +43,72 @@ public class MemberManager {
 		}
 		return ret;
 	}
+	public HashMap<String, Member> getMembers() {
+		return members;
+	}
+	public void setMembers(HashMap<String, Member> members) {
+		this.members = members;
+	}
+	
+	public Member searchMemberByID(String id) {
+		return members.get(id);
+	}
+	public ArrayList<Member> searchMemberByAddress(String address) {
+		Iterator<String> keys=members.keySet().iterator();
+		ArrayList<Member> member=null;
+		if(keys.hasNext())member=new ArrayList<Member>();
+		else return null;
+		
+		while(keys.hasNext()) {
+			String key=keys.next();
+			Member mem=members.get(key);
+			if(mem.getAddress().contains(address)) {
+				member.add(mem);
+			}
+		}
+		return member;
+	}
+	public ArrayList<Member> searchMemberByName(String name){
+		Iterator<String> keys=members.keySet().iterator();
+		ArrayList<Member> member=null;
+		if(keys.hasNext())member=new ArrayList<Member>();
+		else return null;
+		
+		while(keys.hasNext()) {
+			String key=keys.next();
+			Member mem=members.get(key);
+			if(mem.getName().equals(name)) {
+				member.add(mem);
+			}
+		}
+		return member;
+	}
+	public byte modifyMember(String id, Member member) {
+		if(searchMemberByID(member.getId())==null)return 0;
+		
+		this.members.put(id, member);
+		
+		return 1;
+	}
+	public byte modifyPassword(String id,String password) {
+		if(searchMemberByID(id)==null)return 0;
+		
+	
+		return this.members.get(id).modifyPassword(password);
+	}
+	public byte deleteMember(String id) {
+		Member member=searchMemberByID(id);
+		if(member==null)return 0;
+		else {
+			this.members.remove(id);
+		}
+		return 1;
+	}
+	public byte addMember(Member member) {
+		if(searchMemberByID(member.getId())!=null)return 0;
+		
+		this.members.put(member.getId(),member);
+		return 1;
+	}
 
 }
