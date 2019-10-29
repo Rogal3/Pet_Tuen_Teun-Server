006db9dc6212066e016cd9af1ff0e699d31c21a0
rogal3 <cws0188@gmail.com>
2019-10-30 02:17:43 +0900

    add Rating in model package

---
+++ src/main/java/model/Rating.java
@@ -0,0 +1,60 @@
+package model;
+
+public class Rating {
+	private String writer;
+	private String hospital;
+	private int scale;
+	private String content;
+	
+	public Rating() {
+		super();
+	}
+
+	public Rating(String writer, String hospital, int scale, String content) {
+		super();
+		this.writer = writer;
+		this.hospital = hospital;
+		this.scale = scale;
+		this.content = content;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getHospital() {
+		return hospital;
+	}
+
+	public void setHospital(String hospital) {
+		this.hospital = hospital;
+	}
+
+	public int getScale() {
+		return scale;
+	}
+
+	public void setScale(int scale) {
+		this.scale = scale;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public byte modifyScale(int scale) {
+		this.scale = scale;
+		return 1;
+	}
+	public byte modifyContent(String content) {
+		this.content = content;
+		return 1;
+	}
+}
