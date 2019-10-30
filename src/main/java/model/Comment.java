db2d716d4ab5036efa57e12e47c1fdca9d8dacc9
rogal3 <cws0188@gmail.com>
2019-10-30 20:31:51 +0900

    update Comment
    write is wrong word, changed writer

--- src/main/java/model/Comment.java
+++ src/main/java/model/Comment.java
@@ -1,24 +1,24 @@
 package model;
 
 public class Comment {
-	private String write;
+	private String writer;
 	private String content;
 	private String writeTime;
 	
 	public Comment() {
 		super();
 	}
-	public Comment(String write, String content, String writeTime) {
+	public Comment(String writer, String content, String writeTime) {
 		super();
-		this.write = write;
+		this.writer = writer;
 		this.content = content;
 		this.writeTime = writeTime;
 	}
 	public String getWrite() {
-		return write;
+		return writer;
 	}
 	public void setWrite(String write) {
-		this.write = write;
+		this.writer = write;
 	}
 	public String getContent() {
 		return content;
